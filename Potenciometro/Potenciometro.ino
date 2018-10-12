5c37bda5c0411faf5d9d09e2bc9514c665c2ec8a
Nico Ramirez <40794868+NicolasRamirez9@users.noreply.github.com>
2018-10-12 20:20:09 -0300

    Add files via upload

---
+++ Potenciometro/Potenciometro.ino
@@ -0,0 +1,31 @@
+float voltaje;
+float voltaje255;
+
+void setup()
+{
+  pinMode(-9, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  voltaje = analogRead(A0);
+  Serial.println(voltaje);
+  voltaje255=map(voltaje,0,1023,0,255);
+  
+  analogWrite(10,voltaje255);
+  
+  analogWrite(9,voltaje/4);
+  if(voltaje255<100)
+    analogWrite(10,voltaje255);
+  else
+    analogWrite(10,0);
+  
+  if (voltaje255>200)
+    analogWrite(11,voltaje255);
+  else
+    analogWrite(11,0);
+  
+  analogWrite(13,250);
+  
+}
